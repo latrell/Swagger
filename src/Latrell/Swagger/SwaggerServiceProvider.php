cc884b9ab0e829f6f1dd15037fb37c564ac4df13
Latrell Chan <i@latrell.me>
2015-03-08 13:58:15 +0800

    修改为Laravel5支持。

--- src/Latrell/Swagger/SwaggerServiceProvider.php
+++ src/Latrell/Swagger/SwaggerServiceProvider.php
@@ -2,47 +2,31 @@
 namespace Latrell\Swagger;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Config;
 
 class SwaggerServiceProvider extends ServiceProvider
 {
 
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+	/**
+	 * boot process
+	 */
+	public function boot()
+	{
+		if (config('latrell/swagger/config.enable')) {
+			require __DIR__ . '/../../routes.php';
+		}
+	}
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->package('latrell/swagger');
-
-        $this->app->bind('swagger::install', function ($app)
-        {
-            return new Installer();
-        });
-        $this->commands(array(
-            'swagger::install'
-        ));
-
-        if (Config::get('swagger::enable')) {
-            require __DIR__ . '/../../routes.php';
-        }
-    }
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$configPath = __DIR__ . '/../../config/config.php';
+		$this->mergeConfigFrom($configPath, 'latrell/swagger/config');
+		$this->publishes([
+			$configPath => config_path('latrell/swagger/config.php')
+		]);
+	}
 }
