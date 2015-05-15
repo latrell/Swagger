46ee908e34817b7a46538b5e2c9bc993eafd0244
Latrell Chan <i@latrell.me>
2015-05-15 15:08:50 +0800

    更新Swagger-UI为最新版本。

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -2,64 +2,79 @@
 <html>
 <head>
   <title>{{ config('latrell-swagger.title') }}</title>
-  <!-- <link href='https://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'/> -->
-  <link href="{{ asset('vendor/latrell/swagger/css/fonts.css') }}" rel='stylesheet' type='text/css'/>
-  <link href="{{ asset('vendor/latrell/swagger/css/reset.css') }}" media='screen' rel='stylesheet' type='text/css'/>
-  <link href="{{ asset('vendor/latrell/swagger/css/screen.css') }}" media='screen' rel='stylesheet' type='text/css'/>
-  <link href="{{ asset('vendor/latrell/swagger/css/reset.css') }}" media='print' rel='stylesheet' type='text/css'/>
-  <link href="{{ asset('vendor/latrell/swagger/css/screen.css') }}" media='print' rel='stylesheet' type='text/css'/>
-  <script src="{{ asset('vendor/latrell/swagger/lib/shred.bundle.js') }}" type="text/javascript"></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/jquery-1.8.0.min.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/jquery.slideto.min.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/jquery.wiggle.min.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/jquery.ba-bbq.min.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/handlebars-1.0.0.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/underscore-min.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/backbone-min.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/swagger.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/swagger-ui.js') }}" type='text/javascript'></script>
-  <script src="{{ asset('vendor/latrell/swagger/lib/highlight.7.3.pack.js') }}" type='text/javascript'></script>
-
-  <!-- enabling this will enable oauth2 implicit scope support -->
-  <script src="{{ asset('vendor/latrell/swagger/lib/swagger-oauth.js') }}" type='text/javascript'></script>
+  <link rel="icon" type="image/png" href="{{ asset('vendor/latrell/swagger/images/favicon-32x32.png') }}" sizes="32x32" />
+  <link rel="icon" type="image/png" href="{{ asset('vendor/latrell/swagger/images/favicon-16x16.png') }}" sizes="16x16" />
+  <link href='{{ asset('vendor/latrell/swagger/css/typography.css') }}' media='screen' rel='stylesheet' type='text/css'/>
+  <link href='{{ asset('vendor/latrell/swagger/css/reset.css') }}' media='screen' rel='stylesheet' type='text/css'/>
+  <link href='{{ asset('vendor/latrell/swagger/css/screen.css') }}' media='screen' rel='stylesheet' type='text/css'/>
+  <link href='{{ asset('vendor/latrell/swagger/css/reset.css') }}' media='print' rel='stylesheet' type='text/css'/>
+  <link href='{{ asset('vendor/latrell/swagger/css/print.css') }}' media='print' rel='stylesheet' type='text/css'/>
+  <script src='{{ asset('vendor/latrell/swagger/lib/jquery-1.8.0.min.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/jquery.slideto.min.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/jquery.wiggle.min.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/jquery.ba-bbq.min.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/handlebars-2.0.0.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/underscore-min.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/backbone-min.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/swagger-ui.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/highlight.7.3.pack.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/marked.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lib/swagger-oauth.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lang/translator.js') }}' type='text/javascript'></script>
+  <script src='{{ asset('vendor/latrell/swagger/lang/en.js') }}' type='text/javascript'></script>
 
   <script type="text/javascript">
     $(function () {
       window.swaggerUi = new SwaggerUi({
-      url: "{{ URL::route('swagger_docs') }}",
-      dom_id: "swagger-ui-container",
-      supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
-      onComplete: function(swaggerApi, swaggerUi){
-        log("Loaded SwaggerUI");
-
-        if(typeof initOAuth == "function") {
-        	/*
+        url: "{{ URL::route('swagger_docs') }}",
+        dom_id: "swagger-ui-container",
+        supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+        onComplete: function(swaggerApi, swaggerUi){
+          if(typeof initOAuth == "function") {
             initOAuth({
               clientId: "your-client-id",
               realm: "your-realms",
               appName: "your-app-name"
             });
-            */
-        }
-        $('pre code').each(function(i, e) {
-          hljs.highlightBlock(e)
-        });
-      },
-      onFailure: function(data) {
-        log("Unable to Load SwaggerUI");
-      },
-      docExpansion: "none"
-    });
+          }
 
-    $('#input_apiKey').change(function() {
-      var key = $('#input_apiKey')[0].value;
-      log("key: " + key);
-      if(key && key.trim() != "") {
-        log("added key " + key);
-        window.authorizations.add("key", new ApiKeyAuthorization("api_key", key, "query"));
+          $('pre code').each(function(i, e) {
+            hljs.highlightBlock(e)
+          });
+
+          addApiKeyAuthorization();
+        },
+        onFailure: function(data) {
+          log("Unable to Load SwaggerUI");
+        },
+        docExpansion: "none",
+        apisSorter: "alpha"
+      });
+
+      function addApiKeyAuthorization(){
+        var key = encodeURIComponent($('#input_apiKey')[0].value);
+        if(key && key.trim() != "") {
+            var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("api_key", key, "query");
+            window.swaggerUi.api.clientAuthorizations.add("api_key", apiKeyAuth);
+            log("added key " + key);
+        }
       }
-    })
-    window.swaggerUi.load();
+
+      $('#input_apiKey').change(addApiKeyAuthorization);
+
+      // if you have an apiKey you would like to pre-populate on the page for demonstration purposes...
+      /*
+        var apiKey = "myApiKeyXXXX123456789";
+        $('#input_apiKey').val(apiKey);
+      */
+
+      window.swaggerUi.load();
+
+      function log() {
+        if ('console' in window) {
+          console.log.apply(console, arguments);
+        }
+      }
   });
   </script>
 </head>
@@ -67,14 +82,8 @@
 <body class="swagger-section">
 <div id='header'>
   <div class="swagger-ui-wrap">
-    <a id="logo" href="http://swagger.wordnik.com">swagger</a>
+    <a id="logo" href="http://swagger.io">swagger</a>
     <form id='api_selector'>
-      <div class='input icon-btn'>
-        <img id="show-pet-store-icon" src="{{ asset('vendor/latrell/swagger/images/pet_store_api.png') }}" title="Show Swagger Petstore Example Apis">
-      </div>
-      <div class='input icon-btn'>
-        <img id="show-wordnik-dev-icon" src="{{ asset('vendor/latrell/swagger/images/wordnik_api.png') }}" title="Show Wordnik Developer Apis">
-      </div>
       <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
       <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
       <div class='input'><a id="explore" href="#">Explore</a></div>
